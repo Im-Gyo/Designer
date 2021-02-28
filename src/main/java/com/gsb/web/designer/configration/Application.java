c92d22aeae8353b6fb88d81a1a0d799e70e9238c
TaeChan <xocksdlekd@gmail.com>
2021-02-28 23:21:46 +0900

    test

--- src/main/java/com/gsb/web/designer/configration/Application.java
+++ src/main/java/com/gsb/web/designer/configration/Application.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args); 
     }
 }
