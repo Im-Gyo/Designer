94f2e262011db3b76c9971f0e89aa2e68163eeec
TaeChan <xocksdlekd@gmail.com>
2021-02-28 23:15:54 +0900

    서버 구조 세팅

--- src/main/java/com/gsb/web/designer/controller/IndexController.java
+++ src/main/java/com/gsb/web/designer/controller/IndexController.java
@@ -1,4 +1,14 @@
 package com.gsb.web.designer.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 public class IndexController {
+    @RequestMapping("/")
+    public ModelAndView index(){
+        ModelAndView ModelAndView = new ModelAndView();
+        ModelAndView.setViewName("index");
+
+        return ModelAndView;
+    }
 }
