433d9bee812eb503a834ed9992c5e374668c5668
Gyo <jywlsh3@naver.com>
2021-03-03 00:56:52 +0900

    post엔티티 클래스 작성(기본게시판 + 의뢰게시판) PostsRepository 작성 PostsRepositoryTest 에러발생 해결 중

--- src/main/java/com/gsb/web/designer/Repository/PostsRepository.java
+++ src/main/java/com/gsb/web/designer/Repository/PostsRepository.java
@@ -1,4 +1,8 @@
 package com.gsb.web.designer.Repository;
 
-public interface PostsRepository {
+import com.gsb.web.designer.domain.posts.Posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
 }
--- src/main/java/com/gsb/web/designer/domain/posts/Posts.java
+++ src/main/java/com/gsb/web/designer/domain/posts/Posts.java
@@ -1,4 +1,38 @@
 package com.gsb.web.designer.domain.posts;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 300, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
 }
